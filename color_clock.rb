f75508579ae393a06668f190f1b0f7bb97905396
Rahul Horé <hore.rahul@gmail.com>
2014-06-29 23:02:39 -0400

    Adds title to app window
    Also a slight stylistic revision to the line that calculates the RGB values from the 6-digit hex code. I have yet to find documentation on how to display colors using the hex code directly, hence the hex to RGB conversion.

--- color_clock.rb
+++ color_clock.rb
@@ -1,12 +1,12 @@
 require 'green_shoes'
 
-Shoes.app height: 50 , width: 250 do
+Shoes.app title: 'Hex Color Clock', height: 50 , width: 250 do
   flow margin_left: -10 do
     @clock = title 'Color Clock', align: 'center'
     animate do
       time        = Time.new
       format      = time.strftime('%H%M%S')
-      r, g, b     = format.chars.each_slice(2).map{|x|x.join.to_i(16)}
+      r, g, b     = format.chars.each_slice(2).map{ |hms| hms.join.to_i(16) }
       @clock.text = bg(fg(format, rgb(255, 255, 255)), rgb(r, g, b))
     end
   end
