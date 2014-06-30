67d903efcd71b88ef23e2e47bd3be17f818fb967
Rahul Horé <hore.rahul@gmail.com>
2014-06-29 22:07:05 -0400

    Creates a minimalist hex color clock
    Using the Shoes graphics toolkit and the green_shoes gem, I render a clock in HHMMSS format (e.g., 3:50:20 am is presented as 035020) that displays the current local time refreshed every second. The text is white while the background is the hex color that corresponds to the current six-digit hexadecimal shown (e.g., [#035020 is a dark shade of green](http://www.color-hex.com/color/035020)).

---
+++ color_clock.rb
@@ -0,0 +1,13 @@
+require 'green_shoes'
+
+Shoes.app height: 50 , width: 250 do
+  flow margin_left: -10 do
+    @clock = title 'Color Clock', align: 'center'
+    animate do
+      time        = Time.new
+      format      = time.strftime('%H%M%S')
+      r, g, b     = format.chars.each_slice(2).map{|x|x.join.to_i(16)}
+      @clock.text = bg(fg(format, rgb(255, 255, 255)), rgb(r, g, b))
+    end
+  end
+end
